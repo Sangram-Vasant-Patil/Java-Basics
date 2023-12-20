147874ac8ac569e6853013b6d71a3fc1a281f328
Sangram-Vasant-Patil <erpatilsangram@gmail.com>
2023-12-20 18:30:18 +0530

    Lecture2 Code Files

---
+++ Lecture2- Variables in Java/ScannerInput.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+
+public class ScannerInput {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        //nextInt()
+        //nextFloat()
+        System.out.println(name);
+    }
+}
---
+++ Lecture2- Variables in Java/VariablesFile1.java
@@ -0,0 +1,28 @@
+
+
+public class VariablesFile1 {
+    public static void main (String args[]){
+       //Variables
+       int a = 150;
+       int b = 100;
+
+       int sum = a+b;
+       System.out.println(sum);
+
+       int difference = a-b;
+       System.out.println(difference);
+
+       int answer = (a*b)/(a-b);
+       System.out.println(answer);
+
+       int mul = a*b;
+       System.out.println(mul);
+
+       float div = a/b;
+       System.out.println(div);
+
+
+
+       }
+   
+}
