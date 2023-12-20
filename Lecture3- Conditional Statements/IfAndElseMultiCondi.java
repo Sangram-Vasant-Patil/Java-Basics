720018acc5f2c6eb5a19716347f64cfbf0033a78
Sangram-Vasant-Patil <erpatilsangram@gmail.com>
2023-12-20 20:11:10 +0530

    Lecture3 Code Files

---
+++ Lecture3- Conditional Statements/IfAndElseMultiCondi.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class IfAndElseMultiCondi {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    if(a==b)
+    {
+        System.out.println("a and b are Equal");
+    }
+    else
+    {
+        if (a > b) {
+            System.out.println("a greater then b");
+        } else {
+            System.out.println("a is smaller then b");
+        }
+    }
+}
+}
