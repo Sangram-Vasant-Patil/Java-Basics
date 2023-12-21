e024deae5a407f825f1f8fa9232c1845a6ba0630
Sangram-Vasant-Patil <erpatilsangram@gmail.com>
2023-12-21 15:08:03 +0530

    Lecture4 Code Files

---
+++ Lecture4- Loops in Java/ForLoopNumber.java
@@ -0,0 +1,13 @@
+public class ForLoopNumber {
+    public static void main(String[] args) {
+        //counter++ =>> counter=counter+1 ->>Increment operator
+        // for(int counter=0;counter<11;counter++){
+        //     //System.out.println(counter);
+        // }
+
+        for(int i=0;i<11;i++) //here i = counter
+        {
+           System.out.print(i+" ");
+        }
+    }
+}
